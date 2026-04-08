8ad00cf07f62ea43c074e1d373d3a458c790ce9e
Gregory Eslinger <gregjesl@gmail.com>
2026-04-08 14:44:38 -0400

    v0.3.4 (#7)
    * Attempted to fix package format issues
    
    * Fixed relative imports
    
    * Add all.py
    
    * Add arguments for prop configuration
    
    * Add KeplerianState support methods
    
    * Added default arguments for script initialization
    
    * Add mission properties
    
    * Fix type warning
    
    * Refactor script
    
    - No longer initialized with values
    - `add_resource` and `set_mission`
    - `create` static method
    
    * Fixed user stop bug
    
    GMATConsole returns 1 when the user adds stop to a mission.
    
    * Update header strings
    
    * Add support for assignment and arrays
    
    * Fix code check
    
    * Bump version
    
    * Remove erroneous script print
    
    * Roll uv.lock
    
    * Added __str__ support for conditions
    
    * Rolled version
    
    * Revert "Rolled version"
    
    This reverts commit 55026f6e629ff40e95927acdddbdafa3817af3ab.
    
    * Roll version
    
    * Add FrameRelativeProperties
    
    * Allow repeat of headers
    
    * Roll version
    
    * Add missing reference frames
    
    * Roll version
    
    * Add angle2 recipe
    
    * Implemented angle2 assignment
    
    * Added back propagation
    
    * Roll version

---
+++ examples/backprop.py
@@ -0,0 +1,45 @@
+from gmython.resources.spacecraft import Spacecraft, KeplerianState
+from gmython.resources.prop import GravityField, ForceModel, Propagator, CelestialBody
+from gmython.mission import Propagate
+from gmython.resources.coordsys import CoordinateSystem, CoordinateSystemAxes
+from gmython.resources.celestial import EARTH, LUNA
+from gmython.resources.report import keplerian_headers, build_report_reader
+from gmython.dispatch import dispatch_instance, DispatchError
+from gmython.script import Script
+
+# Build the coordinate system
+coordsys = CoordinateSystem("MoonMJ2000Eq", LUNA, CoordinateSystemAxes.MJ2000Eq)
+
+# Build the satellite
+state = KeplerianState(2000.0, 0.0, 45.0, 90.0, 135.0, 180.0)
+sat = Spacecraft("Sat1", state, coord_system=coordsys)
+
+# Build the force model
+gravity = GravityField.moon(20, 20)
+model = ForceModel("LunaForceModel", gravity, body=LUNA, point_masses=[EARTH])
+
+# Build the propagator
+prop = Propagator("DefaultProp", model)
+
+# Build the report file
+fields = [sat.elapsed_secs()] + keplerian_headers(sat, coordsys)
+
+with build_report_reader(fields) as report:
+
+    # Build the mission sequence
+    mission = Propagate(prop, [sat], [(sat.elapsed_secs(), -12000.0)], backwards=True)
+
+    # Build the script
+    script = Script.create([coordsys, sat, model, prop, report], [mission])
+
+    with dispatch_instance() as dispatch:
+        try:
+            dispatch.build_and_run(script)
+        except DispatchError as e:
+            with open(e.log) as log:
+                print(log.read())
+
+    # Read out the report
+    data = report.load()
+    for line in data:
+        print(line)
